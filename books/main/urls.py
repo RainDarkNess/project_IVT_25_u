b0614ae6bb14697288f5052a5978c7584db9f185
ReallHotChiliPepe <slavanuzhin@yandex.ru>
2025-02-10 11:41:23 +0400

    <dev-frontend-myTrades> added new page, where will be created "my Trades"

--- books/main/urls.py
+++ books/main/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
                        auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
                        name='password_reset_complete'),
 
-                  # path('profile/<str:username>/', views.profile, name='profile'),
+                  path('myTrades/', views.testMyTrades),
 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
