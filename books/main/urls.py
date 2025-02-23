08ee3a91a6729a773bc1d5dc6f58615bd687ddf2
RainDarkNess <pooplolo52@gmail.com>
2025-02-23 16:55:41 +0400

    <dev-frontend> update index page

--- books/main/urls.py
+++ books/main/urls.py
@@ -20,27 +20,14 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
+                  path("exchange/", views.exchange, name="exchange"),
                   path('', views.test),
-                  # path('lk_user', views.userPageView),
-                  path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login.html"),
-                       name='login'),
-                  path("accounts/password_reset/",
-                       auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"),
-                       name='password_reset'),
-                  path("accounts/logout/", auth_views.LogoutView.as_view(template_name="registration/logged_out.html"),
-                       name='logout'),
-                  path('accounts/password-reset/done/',
-                       auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
-                       name='password_reset_done'),
-                  path('accounts/password-reset-confirm/<uidb64>/<token>/',
-                       auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_done.html"),
-                       name='password_reset_confirm'),
-                  path('accounts/password-reset-complete/',
-                       auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
-                       name='password_reset_complete'),
-
-                  # path('profile/<str:username>/', views.profile, name='profile'),
+                  path('accounts/', include('django.contrib.auth.urls')),
+                  path('user-page/', views.userPage),
+                  path('myTrades/', views.testMyTrades),
+                  path('trading/', views.testTrades),
 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
